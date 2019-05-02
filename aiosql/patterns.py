b81926409893a207ff3811d5e09d83336c92b6e8
Will Vaughn <willieviseoae@gmail.com>
2019-05-02 10:04:20 -0700

    allow variable to be at end of sql statement, w/o ";"

--- aiosql/patterns.py
+++ aiosql/patterns.py
@@ -28,7 +28,7 @@ Pattern: Identifies SQL comments.
 var_pattern = re.compile(
     r'(?P<dblquote>"[^"]+")|'
     r"(?P<quote>\'[^\']+\')|"
-    r"(?P<lead>[^:]):(?P<var_name>[\w-]+)(?P<trail>[^:])"
+    r"(?P<lead>[^:]):(?P<var_name>[\w-]+)(?P<trail>[^:]?)"
 )
 """
 Pattern: Identifies variable definitions in SQL code.
