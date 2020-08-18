6419d2b487972a1332ee7b8cb2f59c4c6053e3a6
Fabien Coelho <fabien@coelho.net>
2020-08-18 13:48:27 +0200

    setup.py file based on toml contents

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import re
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def extract(prop, text):
+    return re.search(f"{prop} = \"(.*)\"", text).group(1)
+
+with open("pyproject.toml") as ph:
+    text = ph.read()
+    name = extract("name", text)
+    version = extract("version", text)
+    url = extract("repository", text)
+    description = extract("description", text)
+
+assert name is not None and version is not None and \
+    url is not None and description is not None
+
+setup(
+    name=name,
+    version=version,
+    packages=find_packages(),
+    author="William Vaughn",
+    author_email="vaughnwilld@gmail.com",
+    url=url,
+    install_requires=[],
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: SQL",
+        "Topic :: Database :: Front-Ends",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
