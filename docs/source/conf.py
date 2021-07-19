9650c64019062b04784eee76f9de49fcf9b6cbba
William Vaughn <vaughnwilld@gmail.com>
2021-07-18 22:05:56 -0700

    add github pages sphinx extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,10 +31,7 @@ release = "3.2.1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx_rtd_theme",
-    "sphinx.ext.autodoc",
-]
+extensions = ["sphinx_rtd_theme", "sphinx.ext.autodoc", "sphinx.ext.githubpages"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
