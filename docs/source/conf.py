c970205ba5b96db4cf9c18f0a54210aff19caa7c
William Vaughn <vaughnwilld@gmail.com>
2021-12-24 15:08:31 -0800

    bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = "William Vaughn <vaughnwilld@gmail.com>"
 github_doc_root = "https://github.com/nackjicholson/aiosql/tree/master/docs/source/"
 
 # The full version, including alpha/beta/rc tags
-release = "3.3.1"
+release = "3.4.0"
 
 
 # -- General configuration ---------------------------------------------------
