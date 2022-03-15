1563935a7d20be2df04008f755ffe7cd21e3d362
Fabien Coelho <fabien@coelho.net>
2022-03-15 08:11:24 +0100

    Reformat __init__.py with "black" for fix github CI.

--- aiosql/__init__.py
+++ aiosql/__init__.py
@@ -2,6 +2,7 @@ from .aiosql import from_path, from_str
 from .exceptions import SQLParseException, SQLLoadException
 
 import pkg_resources as pkg  # type: ignore
+
 __version__ = pkg.require("aiosql")[0].version
 
 __all__ = ["from_path", "from_str", "SQLParseException", "SQLLoadException"]
