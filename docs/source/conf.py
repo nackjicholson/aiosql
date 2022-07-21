981fdd7965f6d72c54a65649c9c09b257616073a
Fabien Coelho <fabien@coelho.net>
2022-07-21 22:28:15 +0200

    update version number for next release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = "William Vaughn <vaughnwilld@gmail.com>"
 github_doc_root = "https://github.com/nackjicholson/aiosql/tree/master/docs/source/"
 
 # The full version, including alpha/beta/rc tags
-release = "4.0"
+release = "5.0"
 
 
 # -- General configuration ---------------------------------------------------
