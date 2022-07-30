8db6a3ac6bde4ffc8daea02b730d3b4e0c12a6c2
Fabien Coelho <fabien@coelho.net>
2022-07-30 08:23:47 +0200

    shorter implementation.

--- aiosql/adapters/mysql.py
+++ aiosql/adapters/mysql.py
@@ -8,17 +8,19 @@ class BrokenMySQLAdapter(PyFormatAdapter):
     See: https://github.com/PyMySQL/PyMySQL/issues/1059
     """
 
-    def _params(self, p):
-        return p if p != () else None
+    def select(self, conn, name, sql, params, rc=None):
+        return super().select(conn, name, sql, params or None, rc)
 
-    def select(self, conn, name, sql, p, rc=None):
-        return super().select(conn, name, sql, self._params(p), rc)
+    def select_one(self, conn, name, sql, params, rc=None):
+        return super().select_one(conn, name, sql, params or None, rc)
 
-    def select_one(self, conn, name, sql, p, rc=None):
-        return super().select_one(conn, name, sql, self._params(p), rc)
+    def select_value(self, conn, name, sql, params):
+        return super().select_value(conn, name, sql, params or None)
 
-    def select_value(self, conn, name, sql, p):
-        return super().select_value(conn, name, sql, self._params(p))
+    def insert_update_delete(self, conn, name, sql, params):
+        return super().insert_update_delete(conn, name, sql, params or None)
 
-    def insert_update_delete(self, conn, name, sql, p):
-        return super().insert_update_delete(conn, name, sql, self._params(p))
+    def insert_returning(self, conn, name, sql, params):  # pragma: no cover
+        return super().insert_returning(conn, name, sql, params or None)
+
+    # left out with params: insert_update_delete_many, select_cursor
