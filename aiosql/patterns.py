3ca2c87692ec0c4a66a71f68a99c368d88bc8401
Fabien Coelho <fabien@coelho.net>
2022-09-03 19:22:29 +0200

    move patterns around
    - unshared patterns are moved where they are used and renamed
    - :XXX is a var reference, is still shared
    - simplify some related code
    - remove unused pattern

--- aiosql/patterns.py
+++ aiosql/patterns.py
@@ -5,42 +5,11 @@ try:
 except ModuleNotFoundError:  # pragma: no cover
     import re  # type: ignore
 
-query_name_definition_pattern = re.compile(r"--\s*name\s*:\s*")
-"""
-Pattern: Identifies name definition comments.
-"""
-
-query_record_class_definition_pattern = re.compile(r"--\s*record_class\s*:\s*(\w+)\s*")
-"""
-Pattern: Identifies record_class definition comments.
-"""
-
-empty_pattern = re.compile(r"^\s*$")
-"""
-Pattern: Identifies empty lines.
-"""
-
-# FIXME this accepts "1st" but seems to reject "é"
-query_nameop_pattern = re.compile(r"^(\w+)(|\^|\$|!|<!|\*!|#)$")
-"""
-Pattern: Enforces names are valid python variable names followed by operation.
-"""
-
-forbidden_query_name_prefix = re.compile(r"^\d")
-"""
-Pattern: not these as a first query name character.
-"""
-
-doc_comment_pattern = re.compile(r"\s*--\s*(.*)$")
-"""
-Pattern: Identifies SQL comments.
-"""
-
-var_pattern = re.compile(
+VAR_REF = re.compile(
     r'(?P<dblquote>"[^"]+")|'
     r"(?P<quote>\'[^\']*\')|"
     r"(?P<lead>[^:]):(?P<var_name>[\w-]+)(?P<trail>[^:]?)"
 )
 """
-Pattern: Identifies variable definitions in SQL code.
+Pattern: Identifies variables in SQL code.
 """
