81afde5d93c4a2bae813484626030d12c3c7c9be
Fabien Coelho <fabien@coelho.net>
2022-09-04 11:11:50 +0200

    next release should be 6.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = "William Vaughn <vaughnwilld@gmail.com>"
 github_doc_root = "https://github.com/nackjicholson/aiosql/tree/master/docs/source/"
 
 # The full version, including alpha/beta/rc tags
-release = "6.3"
+release = "6.4"
 
 
 # -- General configuration ---------------------------------------------------
