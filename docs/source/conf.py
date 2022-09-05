ad9787509cacaa32e3db7e1d554dd7a5d3d9d7fe
Fabien Coelho <fabien@coelho.net>
2022-09-05 20:50:46 +0200

    some minor back compatibility break in 7.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = "William Vaughn <vaughnwilld@gmail.com>"
 github_doc_root = "https://github.com/nackjicholson/aiosql/tree/master/docs/source/"
 
 # The full version, including alpha/beta/rc tags
-release = "6.4"
+release = "7.0"
 
 
 # -- General configuration ---------------------------------------------------
