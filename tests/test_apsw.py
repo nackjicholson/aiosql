c66edfeab8c63a01a4af3b97d8a144a2de68ca75
Fabien Coelho <fabien@coelho.net>
2022-10-03 10:50:15 +0200

    rename to conn

--- tests/test_apsw.py
+++ tests/test_apsw.py
@@ -52,67 +52,67 @@ class APSWConnection(db.Connection):
 
 
 @pytest.fixture()
-def apsw_conn(sqlite3_db_path):
+def conn(sqlite3_db_path):
     conn = APSWConnection(sqlite3_db_path)
     yield conn
     conn.close()
 
 
-def test_record_query(apsw_conn, queries):
-    apsw_conn.setrowtrace(dict_factory)
-    t.run_record_query(apsw_conn, queries)
+def test_record_query(conn, queries):
+    conn.setrowtrace(dict_factory)
+    t.run_record_query(conn, queries)
 
 
-def test_parameterized_query(apsw_conn, queries):
-    t.run_parameterized_query(apsw_conn, queries)
+def test_parameterized_query(conn, queries):
+    t.run_parameterized_query(conn, queries)
 
 
-def test_parameterized_record_query(apsw_conn, queries):
-    apsw_conn.setrowtrace(dict_factory)
-    t.run_parameterized_record_query(apsw_conn, queries, "apsw", t.todate)
+def test_parameterized_record_query(conn, queries):
+    conn.setrowtrace(dict_factory)
+    t.run_parameterized_record_query(conn, queries, "apsw", t.todate)
 
 
-def test_record_class_query(apsw_conn, queries):
-    t.run_record_class_query(apsw_conn, queries, t.todate)
+def test_record_class_query(conn, queries):
+    t.run_record_class_query(conn, queries, t.todate)
 
 
-def test_select_cursor_context_manager(apsw_conn, queries):
-    t.run_select_cursor_context_manager(apsw_conn, queries, t.todate)
+def test_select_cursor_context_manager(conn, queries):
+    t.run_select_cursor_context_manager(conn, queries, t.todate)
 
 
-def test_select_one(apsw_conn, queries):
-    t.run_select_one(apsw_conn, queries)
+def test_select_one(conn, queries):
+    t.run_select_one(conn, queries)
 
 
-def test_select_value(apsw_conn, queries):
-    t.run_select_value(apsw_conn, queries)
+def test_select_value(conn, queries):
+    t.run_select_value(conn, queries)
 
 
-def test_modulo(apsw_conn, queries):
-    actual = queries.blogs.sqlite_get_modulo(apsw_conn, left=7, right=3)
+def test_modulo(conn, queries):
+    actual = queries.blogs.sqlite_get_modulo(conn, left=7, right=3)
     expected = 7 % 3
     assert actual == expected
 
 
 @pytest.mark.skip("APSW does not support RETURNING?")
-def test_insert_returning(apsw_conn, queries):  # pragma: no cover
-    t.run_insert_returning(apsw_conn, queries, DRIVER, t.todate)
+def test_insert_returning(conn, queries):  # pragma: no cover
+    t.run_insert_returning(conn, queries, DRIVER, t.todate)
 
 
-def test_delete(apsw_conn, queries):
-    t.run_delete(apsw_conn, queries, expect=-1)
+def test_delete(conn, queries):
+    t.run_delete(conn, queries, expect=-1)
 
 
-def test_insert_many(apsw_conn, queries):
-    with apsw_conn:
-        t.run_insert_many(apsw_conn, queries, t.todate, expect=-1)
+def test_insert_many(conn, queries):
+    with conn:
+        t.run_insert_many(conn, queries, t.todate, expect=-1)
 
 
-def test_date_time(apsw_conn, queries):
-    t.run_date_time(apsw_conn, queries, DRIVER)
+def test_date_time(conn, queries):
+    t.run_date_time(conn, queries, DRIVER)
 
 
-def test_execute_script(apsw_conn, queries):
-    with apsw_conn:
-        actual = queries.comments.sqlite_create_comments_table(apsw_conn)
+def test_execute_script(conn, queries):
+    with conn:
+        actual = queries.comments.sqlite_create_comments_table(conn)
         assert actual == "DONE"
