8b83a680d08a7546990cdf69cb216d5f5f1333b8
Fabien Coelho <fabien@coelho.net>
2022-10-03 10:50:15 +0200

    rename to conn

--- tests/test_sqlite3.py
+++ tests/test_sqlite3.py
@@ -23,66 +23,66 @@ def queries():
 
 
 @pytest.fixture()
-def sqlite3_conn(sqlite3_db_path):
+def conn(sqlite3_db_path):
     conn = db.connect(sqlite3_db_path)
     yield conn
     conn.close()
 
 
-def test_record_query(sqlite3_conn, queries):
-    sqlite3_conn.row_factory = dict_factory
-    t.run_record_query(sqlite3_conn, queries)
+def test_record_query(conn, queries):
+    conn.row_factory = dict_factory
+    t.run_record_query(conn, queries)
 
 
-def test_parameterized_query(sqlite3_conn, queries):
-    t.run_parameterized_query(sqlite3_conn, queries)
+def test_parameterized_query(conn, queries):
+    t.run_parameterized_query(conn, queries)
 
 
-def test_parameterized_record_query(sqlite3_conn, queries):
-    sqlite3_conn.row_factory = dict_factory
-    t.run_parameterized_record_query(sqlite3_conn, queries, DRIVER, t.todate)
+def test_parameterized_record_query(conn, queries):
+    conn.row_factory = dict_factory
+    t.run_parameterized_record_query(conn, queries, DRIVER, t.todate)
 
 
-def test_record_class_query(sqlite3_conn, queries):
-    t.run_record_class_query(sqlite3_conn, queries, t.todate)
+def test_record_class_query(conn, queries):
+    t.run_record_class_query(conn, queries, t.todate)
 
 
-def test_select_cursor_context_manager(sqlite3_conn, queries):
-    t.run_select_cursor_context_manager(sqlite3_conn, queries, t.todate)
+def test_select_cursor_context_manager(conn, queries):
+    t.run_select_cursor_context_manager(conn, queries, t.todate)
 
 
-def test_select_one(sqlite3_conn, queries):
-    t.run_select_one(sqlite3_conn, queries)
+def test_select_one(conn, queries):
+    t.run_select_one(conn, queries)
 
 
-def test_select_value(sqlite3_conn, queries):
-    t.run_select_value(sqlite3_conn, queries)
+def test_select_value(conn, queries):
+    t.run_select_value(conn, queries)
 
 
-def test_modulo(sqlite3_conn, queries):
-    actual = queries.blogs.sqlite_get_modulo(sqlite3_conn, left=7, right=3)
+def test_modulo(conn, queries):
+    actual = queries.blogs.sqlite_get_modulo(conn, left=7, right=3)
     expected = 7 % 3
     assert actual == expected
 
 
-def test_insert_returning(sqlite3_conn, queries):
-    t.run_insert_returning(sqlite3_conn, queries, DRIVER, t.todate)
+def test_insert_returning(conn, queries):
+    t.run_insert_returning(conn, queries, DRIVER, t.todate)
 
 
-def test_delete(sqlite3_conn, queries):
-    t.run_delete(sqlite3_conn, queries)
+def test_delete(conn, queries):
+    t.run_delete(conn, queries)
 
 
-def test_insert_many(sqlite3_conn, queries):
-    with sqlite3_conn:
-        t.run_insert_many(sqlite3_conn, queries, t.todate)
+def test_insert_many(conn, queries):
+    with conn:
+        t.run_insert_many(conn, queries, t.todate)
 
 
-def test_date_time(sqlite3_conn, queries):
-    t.run_date_time(sqlite3_conn, queries, DRIVER)
+def test_date_time(conn, queries):
+    t.run_date_time(conn, queries, DRIVER)
 
 
-def test_execute_script(sqlite3_conn, queries):
-    with sqlite3_conn:
-        actual = queries.comments.sqlite_create_comments_table(sqlite3_conn)
+def test_execute_script(conn, queries):
+    with conn:
+        actual = queries.comments.sqlite_create_comments_table(conn)
         assert actual == "DONE"
