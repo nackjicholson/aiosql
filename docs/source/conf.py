3715695cf8d9b0386c6fc097008b5d63faefbee9
Fabien Coelho <fabien@coelho.net>
2022-10-07 13:21:11 +0200

    v7.0 (again)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ github_doc_root = "https://github.com/nackjicholson/aiosql/tree/master/docs/sour
 
 # The full version, including alpha/beta/rc tags
 # FIXME should be consistent with "setup.cfg"?
-release = "6.5"
+release = "7.0"
 
 
 # -- General configuration ---------------------------------------------------
