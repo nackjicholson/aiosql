35a0f89109b1ded223e64a8a0a23230ceb10a3ab
Fabien Coelho <fabien@coelho.net>
2023-05-19 09:46:12 +0200

    fix lines for black

--- aiosql/__init__.py
+++ aiosql/__init__.py
@@ -2,6 +2,7 @@ from .aiosql import from_path, from_str, register_adapter
 from .utils import SQLParseException, SQLLoadException
 
 import pkg_resources as pkg  # type: ignore
+
 __version__ = pkg.require("aiosql")[0].version
 
 __all__ = ["from_path", "from_str", "register_adapter", "SQLParseException", "SQLLoadException"]
