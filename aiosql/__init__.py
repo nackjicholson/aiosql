b5090377d4494be0bc80278f252e031db44d44ef
Fabien Coelho <fabien@coelho.net>
2023-05-19 09:32:03 +0200

    drop 3.7 support

--- aiosql/__init__.py
+++ aiosql/__init__.py
@@ -1,15 +1,7 @@
-import sys
-
 from .aiosql import from_path, from_str, register_adapter
 from .utils import SQLParseException, SQLLoadException
 
-if sys.version_info >= (3, 8):
-    from importlib.metadata import version
-
-    __version__ = version("aiosql")
-else:
-    import pkg_resources as pkg  # type: ignore
-
-    __version__ = pkg.require("aiosql")[0].version
+import pkg_resources as pkg  # type: ignore
+__version__ = pkg.require("aiosql")[0].version
 
 __all__ = ["from_path", "from_str", "register_adapter", "SQLParseException", "SQLLoadException"]
