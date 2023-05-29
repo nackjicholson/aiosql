2107605717611f075aa30b9117a973e2e635a394
Fabien Coelho <fabien@coelho.net>
2023-05-29 12:58:46 +0200

    next should be 9.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ github_doc_root = "https://github.com/nackjicholson/aiosql/tree/master/docs/sour
 
 # The full version, including alpha/beta/rc tags
 # FIXME should be consistent with "setup.cfg"?
-release = "9.0"
+release = "9.1"
 
 
 # -- General configuration ---------------------------------------------------
