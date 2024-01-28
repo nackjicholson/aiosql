88817930f66eed669549348657ea6258bc86e7a8
Fabien Coelho <fabien@coelho.net>
2024-01-28 09:00:38 +0100

    improve docstring and comments

--- aiosql/adapters/pyformat.py
+++ aiosql/adapters/pyformat.py
@@ -3,17 +3,19 @@ from ..utils import VAR_REF
 
 
 def _replacer(match):
+    """Regex hook for named to pyformat conversion."""
     gd = match.groupdict()
-    if gd["dquote"] is not None:
+    if gd["dquote"] is not None:  # "..."
         return gd["dquote"]
-    elif gd["squote"] is not None:
+    elif gd["squote"] is not None:  # '...'
         return gd["squote"]
-    else:
+    else:  # :something to %(something)s
         return f'{gd["lead"]}%({gd["var_name"]})s'
 
 
 class PyFormatAdapter(GenericAdapter):
-    """Convert from named to pyformat parameter style"""
+    """Convert from named to pyformat parameter style."""
 
     def process_sql(self, _query_name, _op_type, sql):
+        """From named to pyformat."""
         return VAR_REF.sub(_replacer, sql)
