898b479ac293d082bf49d061727cc86dd5d58354
Fabien Coelho <fabien@coelho.net>
2024-08-18 11:55:30 +0200

    add doc string

--- aiosql/adapters/sqlite3.py
+++ aiosql/adapters/sqlite3.py
@@ -2,7 +2,10 @@ from .generic import GenericAdapter
 
 
 class SQLite3Adapter(GenericAdapter):
-    # overwrites some methods using sqlite3-specific non-standard methods
+    """SQLite3 Adapter for AioSQL.
+
+    Overwrites two methods using sqlite3-specific non-standard methods.
+    """
 
     def insert_returning(self, conn, _query_name, sql, parameters):
         cur = self._cursor(conn)
