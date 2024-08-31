e867a8df5b8597c4fcdf6622dd1d1301089081d8
Fabien Coelho <fabien@coelho.net>
2024-08-31 19:28:45 +0200

    allow options even if pytest-mysql is not installed

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import aiosql
+import utils
 
 from conf_mysql import my_dsn, my_db, my_conn
 from conf_pgsql import pg_conn, pg_params, pg_dsn
@@ -20,6 +21,12 @@ def pytest_addoption(parser):
         choices=["MySQLdb", "mysql.connector", "pymysql", "mariadb"],
         help="which driver to use for creating connections",
     )
+    if not utils.has_pkg("pytest_mysql"):
+        parser.addoption("--mysql-host", type=str, default="localhost")
+        parser.addoption("--mysql-port", type=int, default=3306)
+        parser.addoption("--mysql-user", type=str, default="pytest")
+        parser.addoption("--mysql-passwd", type=str, default="pytest")
+        parser.addoption("--mysql-dbname", type=str, default="pytest")
     # MS SQL Server
     parser.addoption("--mssql-tries", default=1, type=int)
     parser.addoption("--mssql-driver", default="pymssql")
